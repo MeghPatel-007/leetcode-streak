1beab487cec391c2d3df2a813633a8d8aae78f57
MeghPatel-007 <meghpatel0009@gmail.com>
2026-03-09 19:23:53 +0530

    day-2

---
+++ day-2.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *output = new ListNode(0);
+        ListNode *travs = output;
+        int c = 0;
+        while (l1 || l2 || c)
+        {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = x + y + c;
+            c = sum / 10;
+            travs->next = new ListNode(sum % 10);
+            travs = travs->next;
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
+        }
+        return output->next;
+    }
+};
