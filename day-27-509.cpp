6ad485258de05c19bf6d98ddcb67406aa8776c94
MeghPatel-007 <meghpatel0009@gmail.com>
2026-04-03 17:30:01 +0530

    day-27

---
+++ day-27-509.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0)return 0;
+//         if(n==1)return 1;
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)   return 0;
+        if(n == 1)      return 1;
+        int ans = 1;
+        int prev = 0;
+        for(int i = 2;i<=n;i++){
+            int temp = ans;
+            ans = ans+prev;
+            prev = temp;
+        }
+        return ans;
+    }
+};
