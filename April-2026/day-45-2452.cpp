4aafe423dda43572c5886b1a2d76bc5e4cfcb9d2
MeghPatel-007 <meghpatel0009@gmail.com>
2026-04-22 22:14:46 +0530

    day-45

---
+++ April-2026/day-45-2452.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> result;
+
+        for (string &q : queries) {
+            for (string &d : dictionary) {
+                int diff = 0;
+
+                for (int i = 0; i < q.size(); i++) {
+                    if (q[i] != d[i]) diff++;
+                    if (diff > 2) break; // early exit
+                }
+
+                if (diff <= 2) {
+                    result.push_back(q);
+                    break; // no need to check more dictionary words
+                }
+            }
+        }
+
+        return result;
+    }
+};
