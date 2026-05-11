d6c4c0a0078fd2a3d1d4022df1d8ebaf86cdceac
MeghPatel-007 <meghpatel0009@gmail.com>
2026-05-11 19:51:44 +0530

    day-64

---
+++ May-2026/day-64-2553.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+           string num = to_string(nums[i]);
+           for(int j=0;j<num.length();j++){
+                ans.push_back(num[j]-'0');
+           }
+        }
+        return ans;
+    }
+};
