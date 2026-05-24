0f88666f7e809dda1d66ad30bc01ccb30f1088f4
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-05-25 01:12:24 +0530

    day-77

---
+++ May-2026/day-77-1340.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxJumps(vector<int> &arr, int d)
+    {
+        int n = arr.size();
+        vector<int> t(n + 1, -1);
+        vector<pair<int, int>> vec;
+        for (int i = 0; i < n; i++)
+        {
+            vec.push_back({arr[i], i});
+        }
+        sort(vec.begin(), vec.end());
+        for (auto &it : vec)
+        {
+            int val = it.first;
+            int i = it.second;
+            int result = 1;
+            for (int j = i - 1; j >= max(0, i - d); j--)
+            {
+                if (arr[j] >= arr[i])
+                    break;
+                result = max(result, 1 + t[j]);
+            }
+            for (int j = i + 1; j <= min(n - 1, i + d); j++)
+            {
+                if (arr[j] >= arr[i])
+                    break;
+                result = max(result, 1 + t[j]);
+            }
+            t[i] = result;
+        }
+        int answer = 1;
+        for (int i = 0; i < n; i++)
+        {
+            answer = max(answer, t[i]);
+        }
+        return answer;
+    }
+};
