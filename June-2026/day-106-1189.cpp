f14f5d589e34ec6c920b85512b41a74002557ea6
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-06-23 01:21:02 +0530

    day-106

---
+++ June-2026/day-106-1189.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxNumberOfBalloons(string text)
+    {
+        unordered_map<char, int> m;
+        for (char t : text)
+        {
+            m[t]++;
+        }
+        return min({m['b'], m['a'], m['l'] / 2, m['o'] / 2, m['n']});
+    }
+};
