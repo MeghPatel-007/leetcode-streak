dce0eaa01208d338257a60d5fd47dbbbef462cb4
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-07-17 00:36:50 +0530

    day-129

--- July-2026/day-129-198.cpp
+++ July-2026/day-129-198.cpp
@@ -1,16 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int rob(vector<int>& nums) {
+    int rob(vector<int> &nums)
+    {
         int n = nums.size();
         int prev = nums[0];
         int prev2 = 0;
-        for(int i=1;i<n;i++){
+        for (int i = 1; i < n; i++)
+        {
             int take = nums[i];
-            if(i>1)take += prev2;
-            int curr_i = max(take,prev);
+            if (i > 1)
+                take += prev2;
+            int curr_i = max(take, prev);
             prev2 = prev;
             prev = curr_i;
         }
