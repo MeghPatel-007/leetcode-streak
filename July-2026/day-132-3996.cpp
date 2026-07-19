b7141735a4c8e2b6b422cd21cd41fe654df75420
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-07-19 10:43:13 +0530

    day-132

---
+++ July-2026/day-132-3996.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canReach(vector<int> &start, vector<int> &target)
+    {
+        int startSum = start[0] + start[1];
+        int targetSum = target[0] + target[1];
+        return (targetSum - startSum) % 2 == 0 ? true : false;
+    }
+};
