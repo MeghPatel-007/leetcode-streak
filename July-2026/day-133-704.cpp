d12a1dc25541ac5f72710675a33cb973fb366655
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-07-20 23:38:55 +0530

    day-133

--- July-2026/day-133-704.cpp
+++ July-2026/day-133-704.cpp
@@ -1,22 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int> &nums, int target)
-    {
+    int search(vector<int>& nums, int target) {
         int l = 0;
         int r = nums.size();
-        while (l <= r)
-        {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(mid >= nums.size())break;
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] < target)l = mid+1;
+            else r = mid-1;
         }
         return -1;
     }
