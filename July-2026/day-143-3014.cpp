0340bda930b569089f40efc6f3390ec34fecb5c0
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-07-30 19:25:09 +0530

    day-143

---
+++ July-2026/day-143-3014.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumPushes(string word)
+    {
+        int ans = 0;
+        int n = word.length();
+        for (int i = 0; i < n; i++)
+        {
+            ans += i / 8 + 1;
+        }
+        return ans;
+    }
+};
