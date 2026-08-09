ba6b2f4a450f0fd3fb653b9d277d17a6324c8f4e
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-09 15:29:16 +0530

    day-153

---
+++ August-2026/day-153-4014.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    double minPrice(vector<int> &prices, vector<int> &discounts)
+    {
+        sort(begin(prices), end(prices), greater<int>());
+        sort(begin(discounts), end(discounts), greater<int>());
+        int i = 0;
+        int j = 0;
+        int n = prices.size();
+        int m = discounts.size();
+        double sum = 0;
+        while (i < n && j < m)
+        {
+            sum += prices[i] * (100 - discounts[j]) / double(100);
+            i++;
+            j++;
+        }
+        while (i < n)
+        {
+            sum += prices[i];
+            i++;
+        }
+        return sum;
+    }
+};
