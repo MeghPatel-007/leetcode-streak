8b62e2a99b12230a3dca87334440a9a62aba1eed
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-10 22:58:49 +0530

    day-155

---
+++ August-2026/day-155-1510.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool winnerSquareGame(int n)
+    {
+        vector<bool> t(n + 1, false);
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int k = 1; k * k <= i; k++)
+            {
+                if (!t[i - k * k])
+                {
+                    t[i] = true;
+                    break;
+                };
+            }
+        }
+        return t[n];
+    }
+};
