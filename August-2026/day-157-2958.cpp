fda6c2199c521945e84282dac9af8a45509a55b6
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-12 19:20:39 +0530

    day-157

---
+++ August-2026/day-157-2958.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// * sliding window
+class Solution
+{
+public:
+    int maxSubarrayLength(vector<int> &nums, int k)
+    {
+        int i = 0, j = 0, n = nums.size(), result = 0;
+        unordered_map<int, int> m;
+        while (j < n)
+        {
+            m[nums[j]]++;
+            while (m[nums[j]] > k)
+            {
+                m[nums[i]]--;
+                i++;
+            }
+            result = max(result, j - i + 1);
+            j++;
+        }
+        return result;
+    }
+};
