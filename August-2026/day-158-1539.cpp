b53a0e65f0664d4475cc41f2e3e399d6bd37dfee
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-13 23:49:22 +0530

    day-158

---
+++ August-2026/day-158-1539.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// * binary search using missing number
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        int n = arr.size();
+        int l = 0;
+        int r = n - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if (k < missing)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return r + k + 1; // maths is used
+        // => missing = arr[r]-(r+1)
+        // => more = k - missing
+        // => ans = arr[r] + more
+        // =>     = arr[r] + k - arr[r] + r + 1
+        // =>     = k+r+1
+    }
+};
