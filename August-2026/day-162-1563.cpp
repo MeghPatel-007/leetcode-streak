bc70ef9a20bffd0b8261a4e48f13ce8788fded63
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-18 00:34:24 +0530

    day-162

--- August-2026/day-162-1563.cpp
+++ August-2026/day-162-1563.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// * dynamic programming => recursive & memo
 class Solution
 {
 public:
@@ -46,5 +47,48 @@ public:
         }
         memset(t, -1, sizeof(t));
         return solve(cumSum, 0, n - 1);
+    }
+};
+
+// * bottom-up
+class Solution
+{
+public:
+    int stoneGameV(vector<int> &stoneValue)
+    {
+        int n = stoneValue.size();
+        vector<int> cumSum(n, 0);
+        cumSum[0] = stoneValue[0];
+        for (int i = 1; i < n; i++)
+        {
+            cumSum[i] = cumSum[i - 1] + stoneValue[i];
+        }
+        vector<vector<int>> t(n + 1, vector<int>(n + 1, 0));
+        for (int l = n - 1; l >= 0; l--)
+        {
+            for (int r = l + 1; r < n; r++)
+            {
+                int score = 0;
+                for (int mid = l; mid <= r - 1; mid++)
+                {
+                    int leftSum = cumSum[mid] - ((l - 1 >= 0) ? cumSum[l - 1] : 0);
+                    int rightSum = cumSum[r] - cumSum[mid];
+                    if (leftSum < rightSum)
+                    {
+                        score = max(score, leftSum + t[l][mid]);
+                    }
+                    else if (leftSum > rightSum)
+                    {
+                        score = max(score, rightSum + t[mid + 1][r]);
+                    }
+                    else
+                    {
+                        score = max({score, rightSum + t[mid + 1][r], leftSum + t[l][mid]});
+                    }
+                }
+                t[l][r] = score;
+            }
+        }
+        return t[0][n - 1];
     }
 };
