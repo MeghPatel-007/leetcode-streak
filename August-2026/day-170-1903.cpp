b2f8dc1ff42893f1a1a07b1b57baa4ba27316a6c
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-26 21:17:35 +0530

    day-170

---
+++ August-2026/day-170-1903.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// * string
+class Solution
+{
+public:
+    string largestOddNumber(string num)
+    {
+        if (num.back() % 2 == 1)
+            return num;
+        int n = num.length();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if ((num[i] - '0') % 2 == 1)
+            {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
