2df2b7bb1f38534f73f28d620731e640a0c30723
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-28 16:16:39 +0530

    day-172

---
+++ August-2026/day-172-876.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int length(ListNode *head)
+    {
+        ListNode *travs = head;
+        int len = 0;
+        while (travs != nullptr)
+        {
+            len++;
+            travs = travs->next;
+        }
+        return len;
+    }
+    ListNode *middleNode(ListNode *head)
+    {
+        int len = length(head);
+        int mid = len / 2;
+        ListNode *midNode = head;
+        while (mid--)
+        {
+            midNode = midNode->next;
+        }
+        return midNode;
+    }
+};
