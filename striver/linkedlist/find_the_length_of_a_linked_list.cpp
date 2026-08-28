17894fd945ba684e0855744c322204db3ad9aea7
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-28 15:28:09 +0530

    day-172

---
+++ striver/linkedlist/find_the_length_of_a_linked_list.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data = 0, Node *next = nullptr)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+class ll
+{
+public:
+    Node *head;
+    int len;
+    ll(Node *head = new Node())
+    {
+        this->head = head;
+        len = -1;
+    }
+    void insertNode(Node *head, int newData)
+    {
+        this->len++;
+        Node *newNode = new Node(newData);
+        Node *travs = head;
+        while (travs->next != nullptr)
+        {
+            travs = travs->next;
+        }
+        travs->next = newNode;
+    }
+    void printList(Node *head)
+    {
+        Node *travs = head;
+        while (travs != nullptr)
+        {
+            cout << travs->data << " ";
+            travs = travs->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    ll *l1 = new ll();
+    int n = 10;
+    for (int i = 0; i < n; i++)
+    {
+        l1->insertNode(l1->head, i);
+    }
+    l1->printList(l1->head->next);
+    cout << l1->len;
+}
