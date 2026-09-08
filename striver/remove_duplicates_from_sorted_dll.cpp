4ce5f5aa578ef470a3a47c39190831e0b3737fb4
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-09-08 22:19:36 +0530

    day-183

---
+++ striver/remove_duplicates_from_sorted_dll.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class Solution
+{
+public:
+    Node *head = nullptr;
+
+    void insertAtEnd(int value)
+    {
+        Node *newNode = new Node(value);
+
+        if (!head)
+        {
+            head = newNode;
+            return;
+        }
+
+        Node *current = head;
+        while (current->next)
+        {
+            current = current->next;
+        }
+
+        current->next = newNode;
+        newNode->prev = current;
+    }
+
+    Node *removeDuplicates()
+    {
+        if (!head)
+            return nullptr;
+        Node *current = head;
+        while (current != nullptr && current->next != nullptr)
+        {
+            Node *newDistinct = current->next;
+            while (newDistinct != nullptr && newDistinct->data == current->data)
+            {
+                Node *dupNode = newDistinct;
+                newDistinct = newDistinct->next;
+                delete dupNode;
+            }
+            current->next = newDistinct;
+            if (newDistinct != nullptr)
+            {
+                newDistinct->prev = current;
+            }
+            current = current->next;
+        }
+        return head;
+    }
+
+    void printList()
+    {
+        Node *current = head;
+
+        while (current)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<int> values = {1, 2, 2, 2, 3, 4, 4, 5, 5, 6};
+
+    for (int value : values)
+    {
+        sol.insertAtEnd(value);
+    }
+
+    cout << "Original List: ";
+    sol.printList();
+
+    sol.removeDuplicates();
+
+    cout << "After Removing Duplicates (keeping 1 occurrence): ";
+    sol.printList();
+
+    return 0;
+}
