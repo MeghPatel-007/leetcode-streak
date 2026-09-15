c16eb46009944ae37c6ca4b1c12945f760812bf1
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-09-15 23:32:24 +0530

    day-189

---
+++ September-2026/day-189-49.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// * hash map
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> ans;
+
+        for (string &s : strs)
+        {
+            string key = s;
+            sort(key.begin(), key.end());
+            ans[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto &entry : ans)
+        {
+            result.push_back(entry.second);
+        }
+
+        return result;
+    }
+};
